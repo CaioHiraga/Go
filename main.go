97bd759799d19bb36563111ef08fcac55e8e075b
caiohiraga <caiohiraga@gmail.com>
2025-01-13 00:29:13 -0300

    Add values

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go Essentials!")
+	fmt.Println("Hello, Go Essentials!") //String
+
+	fmt.Println(2) //Inteiros
+
+	fmt.Println(2 + 5) // Expressões
+
+	//Booleanos
+	fmt.Println(true)
+	fmt.Println(false)
 
 }
 
