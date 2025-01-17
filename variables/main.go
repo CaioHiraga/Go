8a061f72d41cd58370407a3368da9480faf28731
caiohiraga <caiohiraga@gmail.com>
2025-01-17 08:52:38 -0300

    variables

---
+++ variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := "Hello World" //criação/declaração e atribuição de x -> :=
+	fmt.Println(x)     //referencia a x
+
+	var y string //criação/declaração de y
+	y = "y: Hello World"
+	fmt.Println(y) //referencia a y
+
+	var z, a, b int = 1, 2, 3 //criacao/declaração de n variáveis
+	fmt.Println(z)
+	fmt.Println(a)
+	fmt.Println(b)
+
+	c, d := 3, 5
+	fmt.Println(c)
+	fmt.Println(d)
+
+	// estaticamente tipado + fortemente tipado
+}
