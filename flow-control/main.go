0577b8d5faef06bc190c0a515ae947db7859ea52
caiohiraga <caiohiraga@gmail.com>
2025-01-18 01:15:34 -0300

    loop-inf-switch-case

--- flow-control/main.go
+++ flow-control/main.go
@@ -6,22 +6,51 @@ const MAX int = 100
 
 func main() {
 
-	n := 0          //declarando e atribuindo valor 0 a n
-	expr := n < MAX //declarando expr e atribuindoo valor (n < MAX)
+	n := 0
+
+	expr := n < MAX //declarando expr e atribuindoo valor {n < MAX}
 	if expr {
 		fmt.Printf("%d é menor do que MAX == %d\n", n, MAX)
 	} else {
 		fmt.Printf("%d é maior do que MAX == %d\n", n, MAX)
 	}
 
-	for ; n < MAX; n = n + 1 {
-
+	for n := 0; n < MAX; n++ {
 		if n%2 == 0 {
 			fmt.Printf("%d é par\n", n)
 
 		} else {
 			fmt.Printf("%d é impar\n", n)
 		}
-
 	}
+	// for (inicializacao); (condicao que precisa ser verdade); (incremento)
+	for i := 0; i < MAX; i++ {
+	}
+
+	for {
+		n = n + 1
+		fmt.Println(n)
+		if n > MAX {
+			break
+		}
+	} // loop infinito (não existe while em Go, apenas for infinito)
+
+	switch n < MAX {
+	case true:
+		{
+			fmt.Println("n é menor do que MAX")
+		}
+	case false:
+		{
+			fmt.Println("n não é menor  do que MAX")
+		}
+	}
+
+	switch n {
+	case 100:
+		fmt.Println("hello")
+	default:
+		fmt.Println("não é 100")
+	}
+
 }
